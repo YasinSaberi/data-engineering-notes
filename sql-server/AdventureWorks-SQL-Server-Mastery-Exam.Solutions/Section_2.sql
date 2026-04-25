061275e3a990fa3f96342a107f956d011dfae922
Yasin Saberi <139574694+YasinSaberi@users.noreply.github.com>
2026-04-25 21:50:04 +0330

    Update Section_2.sql

--- sql-server/AdventureWorks-SQL-Server-Mastery-Exam.Solutions/Section_2.sql
+++ sql-server/AdventureWorks-SQL-Server-Mastery-Exam.Solutions/Section_2.sql
@@ -1,1 +1,9 @@
-
+-- 1
+SELECT
+    e.BusinessEntityID AS EmployeeID,
+    e.JobTitle AS EmployeeJobTitle,
+    m.BusinessEntityID AS ManagerID,
+    m.JobTitle AS ManagerJobTitle
+FROM
+    HumanResources.Employee e
+    LEFT JOIN HumanResources.Employee m ON m.OrganizationNode = e.OrganizationNode.GetAncestor(1);
