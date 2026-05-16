dd422975da25fa46af8a05705336ec830227f318
Yasin Saberi <139574694+YasinSaberi@users.noreply.github.com>
2026-05-16 22:47:43 +0330

    Update and rename real_world_task_sol.py to real_world_task1_sol.py

--- Python/real_world_task1_sol.py
+++ Python/real_world_task1_sol.py
@@ -31,29 +31,3 @@ print("total revenue: ", grand_totla)
 print("avrage: ", avg)
 print("\norder per product: ")
 print(product_count)
-
-# TASK 2
-import pandas as pd
-
-# Load
-jf = pd.read_json("users.json")
-
-# IDENTIFICATION
-print("--- DATA HEALTH CHECK ---")
-print(f"Total Records: {len(jf)}")
-print(jf.isnull().sum()) 
-print(jf.dtypes)
-
-# ANALYSIS 
-unique_countries = jf['country'].dropna().unique()
-print(f"\nUnique Countries ({len(unique_countries)}): {unique_countries}")
-
-# DATA CLEANING
-jf.drop_duplicates(inplace=True)
-jf['age'] = jf['age'].fillna(jf['age'].median()).astype(int)
-jf['country'] = jf['country'].fillna("Unknown")
-
-# REPORT
-print("\n--- CLEANED DATA PREVIEW ---")
-print(jf.head())
-print(jf.describe())
